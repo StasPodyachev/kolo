9292c2ad5f168fe047f42a27e7db5e032aa3311d
vlad-shulzhenko <shulzhenkovlad01@gmail.com>
2023-01-22 16:48:11 +0200

    feat(structure): updated structure

---
+++ client/app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import {Box, Text} from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <main>
+      <Box>
+        <Text>
+          Hello, I am Kolo
+        </Text>
+      </Box>
+    </main>
+  )
+}
