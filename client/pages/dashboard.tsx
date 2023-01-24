28b71c829d440952706523f582a4bfc68d57acb7
Novgorodskii Igor <novgorodskiji@gmail.com>
2023-01-24 10:51:15 +0200

    add sdk

---
+++ client/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import {Box, Heading, Text} from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+      <Heading variant="h1">Hello kolo</Heading>
+      <Text>Me here</Text>
+    </Box>
+  )
+}
+export default Home;
