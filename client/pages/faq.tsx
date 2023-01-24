66cac5163293e1a6d06875755e751593d733111c
Novgorodskii Igor <novgorodskiji@gmail.com>
2023-01-24 11:10:18 +0200

    add faq

---
+++ client/pages/faq.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import {Box, Heading, Text} from "@chakra-ui/react";
+
+const FAQ: NextPage = () => {
+  return (
+    <Box>
+      <Heading variant="h1">FAQ</Heading>
+      <Text>FAQ</Text>
+    </Box>
+  )
+}
+export default FAQ;
