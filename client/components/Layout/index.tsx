0a807f555a9bbe0f498dfe1ceacbe9f62f5840fc
vlad-shulzhenko <shulzhenkovlad01@gmail.com>
2023-01-24 11:10:01 +0200

    feat(product): made product page

---
+++ client/components/Layout/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const Layout: NextPage = () => {
+  return (
+    <div>
+      <span>layout</span>
+    </div>
+  );
+};
---
+++ client/pages/product.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Product: NextPage = () => {
+  return (
+    <div>
+      <span>hello</span>
+    </div>
+  );
+};
+
+export default Product;
