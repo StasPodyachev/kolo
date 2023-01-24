d57eeadd2cfe0d86261af9f3e6c1049e1f0c26cf
Novgorodskii Igor <novgorodskiji@gmail.com>
2023-01-24 11:25:43 +0200

    delete product page

--- client/pages/products/[productId].tsx
+++ client/pages/products/[productId].tsx
@@ -1,11 +1,9 @@
 import {NextPage} from "next";
-import {Box, Heading} from "@chakra-ui/react";
+import {Box,Heading} from "@chakra-ui/react";
 
 const Product: NextPage = () => {
   return (
-    <Box>
-      <Heading variant="h1">Hello Product</Heading>
-    </Box>
+    <Box><Heading variant="h1">Hello Product</Heading></Box>
   )
 }
 export default Product;
