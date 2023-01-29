ebaddbf9deb14b8620200fe1e2d96901ac7df7e8
Novgorodskii Igor <novgorodskiji@gmail.com>
2023-01-29 18:37:22 +0200

    update

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,15 +4,29 @@ import ItemCard from "@/components/Home/ItemCard";
 import Layout from "@/components/Layout";
 import { auctionItems } from "@/constants/shared";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IAuctionItem } from "@/types";
+import { useContractRead } from "wagmi";
+import ABI_FACTORY from '../contracts/abi/Factory.json' 
+import addresses from "@/contracts/addresses";
 
 const Home: NextPage = () => {
+  const [ products, setProducts ] = useState([])
   const [startCount, setStartCount] = useState(0);
   const [count, setCount] = useState(6);
   const [items, setItems] = useState<IAuctionItem[]>(
     auctionItems.slice(startCount, count + 1)
   );
+
+  const { data, isError, isLoading } = useContractRead({
+    address: addresses[0].address as `0x${string}`,
+    abi: ABI_FACTORY,
+    functionName: 'getAllDeals',
+  })
+
+  useEffect(() => {
+    console.log(data, 'data');
+  }, [data])
 
   const getMoreItems = () => {
     setStartCount(count + 6);
