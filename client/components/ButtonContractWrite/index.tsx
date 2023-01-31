ca6cb1fbeac82d8b10be92df1986ec3b2169f83e
Novgorodskii Igor <novgorodskiji@gmail.com>
2023-01-31 14:05:54 +0200

    update files

--- client/components/ButtonContractWrite/index.tsx
+++ client/components/ButtonContractWrite/index.tsx
@@ -15,12 +15,16 @@ interface IProps {
 
 const ButtonContractWrite = ({ title, address, abi, method, parrams }: IProps) => {
   const { name, description, priceStart, priceForceStop, dateExpire, cid, collateral} = parrams
-  console.log();
+  console.log({
+    name, description, priceStart, priceForceStop, dateExpire, cid, collateral
+  });
   const date = new Date(dateExpire);
   const newDateExpire = date.getTime();
   const newPriceStart = BigInt(new BigDecimal(priceStart).mul(BIG_1E18 + "").toFixed(0)) + ""
   const newForceStop = BigInt(new BigDecimal(priceForceStop).mul(BIG_1E18 + "").toFixed(0)) + ""
   const newCollateral = BigInt(new BigDecimal(collateral).mul(BIG_1E18 + "").toFixed(0)) + ""
+  // console.log();
+  
   let newCid = web3.utils.asciiToHex(cid)  
   const { config } = usePrepareContractWrite({
     address,
