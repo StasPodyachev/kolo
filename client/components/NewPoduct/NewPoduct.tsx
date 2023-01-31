8d69bf1d699ca2cf7bcdb2d8bb6da4b1db5e2446
Novgorodskii Igor <novgorodskiji@gmail.com>
2023-01-31 14:47:19 +0200

    update item

--- client/components/NewPoduct/NewPoduct.tsx
+++ client/components/NewPoduct/NewPoduct.tsx
@@ -472,6 +472,7 @@ const NewPoduct = () => {
                   priceForceStop: forceStopPrice,
                   dateExpire: stopDate,
                   cid,
+                  collateral: myCollateral
                 }
               }
               isDisabled={!access &&
