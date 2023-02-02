a252ce3731fbc8bb6e0ea65fd24d5b8409876f2a
vlad-shulzhenko <shulzhenkovlad01@gmail.com>
2023-02-02 15:54:48 +0200

    fixed dashboard accordion

--- client/components/ui/AuctionItemsAccordion/index.tsx
+++ client/components/ui/AuctionItemsAccordion/index.tsx
@@ -36,7 +36,7 @@ const AuctionItemAccordion = ({ deals }: IProps) => {
 
   return (
     <Accordion mt="16px" index={accordionIndex}>
-      {deals?.map((item) => {
+      {deals?.map((item, index) => {
         return (
           <AccordionItem
             border="1px solid"
@@ -150,12 +150,13 @@ const AuctionItemAccordion = ({ deals }: IProps) => {
                   <AccordionButton
                     cursor="pointer"
                     onClick={(event) => {
+                      console.log('id', item.id, typeof item.id);
                       event.preventDefault();
                       setNumberOfClicks(1);
-                      setAccordionIndex(item.id - 1);
+                      setAccordionIndex(index);
                       if (numberOfClicks % 2 === 1) {
-                        if (item.id !== accordionIndex + 1) {
-                          setAccordionIndex(item.id - 1);
+                        if (index !== accordionIndex) {
+                          setAccordionIndex(index);
                           setNumberOfClicks(1);
                         } else {
                           setNumberOfClicks(0);
@@ -175,7 +176,7 @@ const AuctionItemAccordion = ({ deals }: IProps) => {
                 </Flex>
               </Flex>
             </Link>
-            <AccordionPanel bg="inherit" p="16px 20px 18px 36px">
+            <AccordionPanel dis bg="inherit" p="16px 20px 18px 36px">
               <Text textStyle="smallText" color="white">
                 Description:&nbsp;{item.description}
               </Text>
