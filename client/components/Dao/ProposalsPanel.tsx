fa9ed0ea0d74634ba7529b57e80f033d2b02e5e8
vlad-shulzhenko <shulzhenkovlad01@gmail.com>
2023-02-06 01:45:59 +0200

    added faq info

--- client/components/Dao/ProposalsPanel.tsx
+++ client/components/Dao/ProposalsPanel.tsx
@@ -14,7 +14,7 @@ const ProposalsPanel = ({ setIndex }: IProps) => {
     <TabPanel p={0}>
       <Flex flexDir="column" gap="20px">
         <Blocks items={ProposalsBlocks} />
-        <Heading>Recent Proposals</Heading>
+        <Heading variant="h3" color="white">Recent Proposals</Heading>
         <Flex flexDir="column" gap="36px">
           {ProposalsItems.map((item: IProposalItem) => (
             <ProposalItem
